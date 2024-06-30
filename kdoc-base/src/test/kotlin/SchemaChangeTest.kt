14fe83ad6b0d5cb651e6c63e0e5efa17289c651a
Fernando Sanchez <perracolabs@gmail.com>
2024-06-30 10:27:07 +0200

    Cleanup

--- kdoc-base/src/test/kotlin/SchemaChangeTest.kt
+++ kdoc-base/src/test/kotlin/SchemaChangeTest.kt
@@ -7,6 +7,7 @@ import org.jetbrains.exposed.sql.Schema
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.jetbrains.exposed.sql.transactions.transaction
+import kotlin.system.measureTimeMillis
 import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
 import kotlin.test.Test
@@ -36,26 +37,25 @@ class SchemaJoinTest {
     fun testMultipleSchemaChangesTiming() {
         val schemas: List<Schema> = List(size = 20) { Schema(name = "SCHEMA_$it") } // Create 10 different schemas.
 
-        val startTime: Long = System.nanoTime() // Start time measurement.
         var tested = 0
 
-        transaction {
-            schemas.forEach { schema ->
-                SchemaUtils.createSchema(schema)
-            }
-
-            repeat(times = 500) {
+        val duration: Long = measureTimeMillis {
+            transaction {
                 schemas.forEach { schema ->
-                    tested++
-                    println("Testing schema: ${schema.identifier}")
-                    SchemaUtils.setSchema(schema = schema)
-                    assertEquals(expected = schema.identifier, actual = TransactionManager.current().connection.schema)
+                    SchemaUtils.createSchema(schema)
+                }
+
+                repeat(times = 500) {
+                    schemas.forEach { schema ->
+                        tested++
+                        println("Testing schema: ${schema.identifier}")
+                        SchemaUtils.setSchema(schema = schema)
+                        assertEquals(expected = schema.identifier, actual = TransactionManager.current().connection.schema)
+                    }
                 }
             }
         }
 
-        val endTime: Long = System.nanoTime() // End time measurement.
-        val duration: Long = (endTime - startTime) / 1_000_000 // Convert nanoseconds to milliseconds.
         println("Total time for schema changes: ${duration}ms. Tested $tested schema changes.")
     }
 }
