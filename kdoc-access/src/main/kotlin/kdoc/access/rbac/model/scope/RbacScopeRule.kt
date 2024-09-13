909450b00156118a3987fa7a2cb5e058ed187793
Fernando Sanchez <perracolabs@gmail.com>
2024-09-13 21:53:43 +0200

    Cleanup

--- kdoc-access/src/main/kotlin/kdoc/access/rbac/model/scope/RbacScopeRule.kt
+++ kdoc-access/src/main/kotlin/kdoc/access/rbac/model/scope/RbacScopeRule.kt
@@ -15,13 +15,13 @@ import kotlinx.serialization.Serializable
 import org.jetbrains.exposed.sql.ResultRow
 
 /**
- * Represents a single RBAC scope rule for a concrete [RbacRole].
+ * Represents a concrete RBAC scope rule for a concrete [RbacRole].
  *
  * A scope can be any concept: a database table, a REST endpoint, a UI element, etc.
  * Is up to the designer to define what a scope is, and act accordingly when its
  * associated RBAC rule is verified.
  *
- * @property id The unique id of the scope rule record.
+ * @property id The unique id of the [RbacScopeRule] record.
  * @property roleId The associated parent [RbacRole] id.
  * @property scope The [RbacScope] to which the scope rule belong.
  * @property accessLevel The required [RbacAccessLevel] for the [RbacScope].
