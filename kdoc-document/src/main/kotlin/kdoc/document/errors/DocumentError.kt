f3e9119ced658a51c388a3fe5ac42cc911ef0506
Fernando Sanchez <perracolabs@gmail.com>
2024-09-14 18:14:14 +0200

    Improved app exceptions

--- kdoc-document/src/main/kotlin/kdoc/document/errors/DocumentError.kt
+++ kdoc-document/src/main/kotlin/kdoc/document/errors/DocumentError.kt
@@ -11,22 +11,22 @@ import kotlin.uuid.Uuid
 /**
  * Concrete errors for the Document domain.
  *
- * @property status The [HttpStatusCode] associated with this error.
- * @property code A unique code identifying the type of error.
+ * @property statusCode The [HttpStatusCode] associated with this error.
+ * @property errorCode A unique code identifying the type of error.
  * @property description A human-readable description of the error.
  * @property reason An optional human-readable reason for the exception, providing more context.
  * @property cause The underlying cause of the exception, if any.
  */
 internal sealed class DocumentError(
-    status: HttpStatusCode,
-    code: String,
+    statusCode: HttpStatusCode,
+    errorCode: String,
     description: String,
     reason: String? = null,
     cause: Throwable? = null
 ) : AppException(
-    status = status,
+    statusCode = statusCode,
+    errorCode = errorCode,
     context = "DOCUMENT",
-    code = code,
     description = description,
     reason = reason,
     cause = cause
@@ -37,12 +37,17 @@ internal sealed class DocumentError(
      * @param documentId The document id that was not found.
      */
     class DocumentNotFound(documentId: Uuid, reason: String? = null, cause: Throwable? = null) : DocumentError(
-        status = HttpStatusCode.NotFound,
-        code = "DOCUMENT_NOT_FOUND",
+        statusCode = STATUS_CODE,
+        errorCode = ERROR_CODE,
         description = "Document not found. Document Id: $documentId",
         reason = reason,
         cause = cause
-    )
+    ) {
+        companion object {
+            val STATUS_CODE: HttpStatusCode = HttpStatusCode.NotFound
+            const val ERROR_CODE: String = "DOCUMENT_NOT_FOUND"
+        }
+    }
 
     /**
      * Error for when no document has been provided to upload.
@@ -50,12 +55,17 @@ internal sealed class DocumentError(
      * @param ownerId The owner id of the document.
      */
     class NoDocumentProvided(ownerId: Uuid, reason: String? = null, cause: Throwable? = null) : DocumentError(
-        status = HttpStatusCode.BadRequest,
-        code = "NO_DOCUMENT_PROVIDED",
+        statusCode = STATUS_CODE,
+        errorCode = ERROR_CODE,
         description = "No document provided to upload. Owner Id: $ownerId",
         reason = reason,
         cause = cause
-    )
+    ) {
+        companion object {
+            val STATUS_CODE: HttpStatusCode = HttpStatusCode.BadRequest
+            const val ERROR_CODE: String = "NO_DOCUMENT_PROVIDED"
+        }
+    }
 
     /**
      * Error for when there was an issue streaming a download.
@@ -63,12 +73,17 @@ internal sealed class DocumentError(
      * @param ownerId The owner id of the document.
      */
     class FailedToStreamDownload(ownerId: Uuid, reason: String? = null, cause: Throwable? = null) : DocumentError(
-        status = HttpStatusCode.BadRequest,
-        code = "FAILED_TO_STREAM_DOWNLOAD",
+        statusCode = STATUS_CODE,
+        errorCode = ERROR_CODE,
         description = "Failed to stream download. Owner Id: $ownerId",
         reason = reason,
         cause = cause
-    )
+    ) {
+        companion object {
+            val STATUS_CODE: HttpStatusCode = HttpStatusCode.BadRequest
+            const val ERROR_CODE: String = "FAILED_TO_STREAM_DOWNLOAD"
+        }
+    }
 
     /**
      * Error for when there was an issue persisting an upload.
@@ -76,10 +91,15 @@ internal sealed class DocumentError(
      * @param ownerId The owner id of the document.
      */
     class FailedToPersistUpload(ownerId: Uuid, reason: String? = null, cause: Throwable? = null) : DocumentError(
-        status = HttpStatusCode.BadRequest,
-        code = "FAILED_TO_PERSIST_UPLOAD",
+        statusCode = STATUS_CODE,
+        errorCode = ERROR_CODE,
         description = "Failed to persist upload. Owner Id: $ownerId",
         reason = reason,
         cause = cause
-    )
+    ) {
+        companion object {
+            val STATUS_CODE: HttpStatusCode = HttpStatusCode.BadRequest
+            const val ERROR_CODE: String = "FAILED_TO_PERSIST_UPLOAD"
+        }
+    }
 }
